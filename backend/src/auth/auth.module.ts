35a555bd5c185276ccb58e660d394311c177513a
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 05:39:44 +0900

    fix: auth module 설정 완료

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { User } from 'src/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    MikroOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {expiresIn: '7d'},
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
