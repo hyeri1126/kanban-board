45ad254f45c2fb3e4a548cc1096281605d7dcc62
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 18:21:24 +0900

    feat: TasksController - 일감 API 엔드포인트 구현

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Post, Body, Patch, Param, Request, Delete } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskResponseDto } from './dto/task-response.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+@UseGuards(JwtAuthGuard)
+export class TasksController {
+    constructor(private readonly tasksService: TasksService){}
+
+    // GET /tasks - 조회
+    @Get()
+    async findAll(@Request() req): Promise<TaskResponseDto[]>{
+        return this.tasksService.findAll(req.user.id);
+    }
+    // POST /tasks - 생성
+    @Post()
+    async create(
+        @Body() createTaskDto: CreateTaskDto,
+        @Request() req,
+    ): Promise<TaskResponseDto> {
+        return this.tasksService.create(createTaskDto, req.user);
+    }
+
+    // PATCH /tasks/:id - 수정 
+    @Patch(':id')
+    async update(
+        @Param('id') id:string,
+        @Body() updateTaskDto: UpdateTaskDto,
+        @Request() req,
+    ): Promise<TaskResponseDto> {
+        return this.tasksService.update(id,updateTaskDto, req.user)
+    }
+
+    // DELETE /tasks/:id - 삭제 
+    @Delete(':id')
+    async delete(
+        @Param('id') id:string,
+        @Request() req,
+    ): Promise<void> {
+        return this.tasksService.delete(id, req.user.id)
+    }
+
+}
