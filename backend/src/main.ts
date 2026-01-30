25384da8186c94510994ad4c3114f61c9533c1e7
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 17:35:51 +0900

    feat: ValidationPiPe 추가 - for DTO 검증

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS 설정
   app.enableCors({
     origin: 'http://localhost:3001',
     credentials:true,
   })
+
+  // Validation Pipe (DTO 검증)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
 
   await app.listen(3000);
 }
