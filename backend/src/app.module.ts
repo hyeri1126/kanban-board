c6b884562c64ee96144e81094c32045ebc7b1993
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 17:26:28 +0900

    refactor: ConfigModule로 환경변수 관리 개선

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 import config from './mikro-orm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MikroOrmModule.forRoot(config),
     AuthModule,
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
