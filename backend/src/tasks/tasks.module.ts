d7ed6d3eaf2484a02ba3490c4a22aa732a6a44d6
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 19:30:00 +0900

    feat: Tasks 모듈 설정

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Task } from '../entities/task.entity';
 
 @Module({
+  imports: [
+    MikroOrmModule.forFeature([Task]),
+  ],
   controllers: [TasksController],
   providers: [TasksService]
 })
