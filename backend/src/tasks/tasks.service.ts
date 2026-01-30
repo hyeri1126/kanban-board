b524556a334de559ba8d8d4168317979d01d414a
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 18:20:50 +0900

    feat: TasksService - 일감 CRUD 로직 구현 (findAll, create, update, delete)

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { EntityManager } from '@mikro-orm/postgresql';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { Task } from 'src/entities/task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { User } from 'src/entities/user.entity';
+import { TaskStatus } from 'src/enums/task-status.enum';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(private readonly em: EntityManager){}
+
+    // 조회
+    async findAll(userId: string): Promise<Task[]> {
+        return this.em.find(
+            Task,
+            {creator: {id:userId}},
+            {populate: ['creator']}
+        )
+    }
+
+    // 생성
+    async create(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+        const {title, status, position} = createTaskDto;
+
+        // task 생성 및 저장
+        const task = new Task(title, user, status || TaskStatus.TODO, position || 0);
+        this.em.persist(task);
+        await this.em.flush();
+
+        return task;
+    } 
+
+    // 수정
+    async update(
+        id: string,
+        updateTaskDto: UpdateTaskDto,
+        user: User
+    ): Promise<Task> {
+        const task = await this.em.findOne(Task, {id}, {populate: ['creator']});
+
+        if(!task){
+            throw new NotFoundException('일감을 찾을 수 없습니다.'); // 404 에러 
+        }
+
+        // 본인이 생성한 일감만 수정 가능(권한 체크)
+        if(task.creator.id !== user.id){    
+            throw new ForbiddenException('권한이 없습니다. 본인의 일감만 수정할 수 있습니다.'); // 403 에러 
+        }
+
+        // 수정
+        if(updateTaskDto.title) task.title = updateTaskDto.title;
+        if(updateTaskDto.status) task.status = updateTaskDto.status;
+        if(updateTaskDto.position !== undefined) task.position = updateTaskDto.position;
+
+        await this.em.flush();
+
+        return task;
+
+    }
+
+    // 삭제 
+    async delete(id: string, userId: string): Promise<void> {
+        const task = await this.em.findOne(Task, {id}, {populate: ['creator']} )
+
+        if(!task){
+            throw new NotFoundException('일감을 찾을 수 없습니다.')
+        }
+
+        // 본인의 일감만 삭제 가능(권한 체크)
+        if (task.creator.id !== userId) {
+            throw new ForbiddenException('권한이 없습니다. 본인의 일감만 삭제할 수 있습니다.');
+        }
+
+        this.em.remove(task);
+        await this.em.flush();
+    }
+}
+
