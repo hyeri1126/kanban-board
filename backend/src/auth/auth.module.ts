5b579a0787ce137bb31e33ce82b94cbf99156187
hyeri1126 <hyeri1126@ewhain.net>
2026-01-30 17:00:50 +0900

    fix: AuthModule에 JwtStrategy 등록

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,20 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { User } from 'src/entities/user.entity';
+import { User } from '../entities/user.entity'
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports:[
     MikroOrmModule.forFeature([User]),
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
+      secret: process.env.JWT_SECRET, 
       signOptions: {expiresIn: '7d'},
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy, // DI 등록
+  ],
+  exports: [AuthService],
 })
 export class AuthModule {}
